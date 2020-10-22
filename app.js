ae446ec5d1b83b91702cba0cfface21f1024c2a0
rochac2lee <cleberli.rocha@gmail.com>
2020-10-22 12:48:16 -0300

    Atualização  de um ou mais documentos

--- app.js
+++ app.js
@@ -19,15 +19,16 @@ MongoClient.connect(servidor, function(erro, db) {
     var colecao = db.collection("alunos");
 
     colecao.insertOne(estudante, function(erro, resultado) {
-        if (erro)
+    if (erro)
         console.log("Erro ao inserir documento: " + erro);
     else
         console.log("Documento inserido com sucesso!");
 
     });
     
-    
     */
+
+    /* Insere vários registros de uma vez
 
     var alunos = [
         { nome: "Cleberli", curso: "Tads19", amigos: ["Lucas Galdino", "Bruna Letícia"] },
@@ -38,12 +39,52 @@ MongoClient.connect(servidor, function(erro, db) {
     var colecao = db.collection("alunos");
 
     colecao.insertMany(alunos, function(erro, resultado) {
-        if (erro)
+    if (erro)
         console.log("Erro ao inserir documento: " + erro);
     else
         console.log("Foram inseridos " + resultado.insertedCount + " alunos!");
 
     });
+
+    */
+
+    /* Atualiza um documento
+    
+    var estudante = {
+        nome: "Cleberli da Rocha",
+        curso: "Tads19",
+        amigos: ["Lucas Galdino", "Yan Ricardo Mendes"]
+    };
+
+    var colecao = db.collection("alunos");
+
+    var filtro = { nome: 'Cleberli'}; 
+
+    colecao.updateOne(filtro, estudante, function(erro, resultado) {
+    if (erro)
+        console.log("Erro ao atualizar documento: " + erro);
+    else
+        console.log("Documento atualizado com sucesso!");
+
+    });
+    
+    */
+
+    var colecao = db.collection("alunos");
+
+    var alteracao = { $set : { matriculado : true } };
+
+    var filtro = { };
+
+    colecao.updateMany(filtro, alteracao, function(erro, resultado) {
+    if (erro)
+        console.log("Erro ao atualizar documentos: " + erro);
+    else
+        console.log("Documentos atualizados com sucesso!");
+
+    });
+    
+
 
     db.close();
 });
